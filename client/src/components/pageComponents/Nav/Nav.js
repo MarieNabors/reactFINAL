cdb5deee6ca19cd6364c868cc76c752d0648a705
Marie Nabors <marienabors@thefirstacademy.org>
2018-05-03 17:54:24 -0400

    updated nav bar

--- client/src/components/pageComponents/Nav/Nav.js
+++ client/src/components/pageComponents/Nav/Nav.js
@@ -8,49 +8,57 @@ import "./Nav.css";
 
 const Nav = () => (
 
-<nav className="navbar navbar-default navbar-fixed-top">
-  <div className="navbar-brand">
-  </div>
-    <div className="container-fluid">
-      <div className="navbar-header">
-          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
+  const Nav = () => (
 
-        <div className="collapse navbar-collapse" id="myNavbar">
-          <ul className="nav navbar-nav navbar-right">
-            <li><a href="/home">HOME</a></li>
-            <li><a href="#memberships">MEMBERSHIP</a></li>
-            <li><a href="#newEvents">EVENTS</a></li>
-            <li><a href="#boardmembers">LEADERSHIP</a></li>
-            <li><a href="#contact">CONTACT</a></li>
+  <nav className="navbar navbar-default navbar-fixed-top">
+    <div className="navbar-brand">
+    </div>
+      <div className="container-fluid">
+        <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
 
-            <li className="dropdown">
-              <a className="dropdown-toggle" data-toggle="dropdown" href="">AWARDS
-              <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="http://www.endo-nurses.org/2018-ENS-Grant-Awards">ENS Poster Award</a></li>
-                  <li><a href="http://www.endo-nurses.org/2018-Betsy-Love-McClung-Award">Betsy Love McClung Award</a></li>
-                  <li><a href="http://www.endo-nurses.org/page-1428935">Media</a></li>
-                </ul>
-            </li>
-            <li className="dropdown">
-              <a className="dropdown-toggle" data-toggle="dropdown" href="#">GRANTS
-              <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="http://www.endo-nurses.org/ENS-2018">Clinical Project Grant</a></li>
-                  <li><a href="http://www.endo-nurses.org/ENS-2018">Clinical Research Grant</a></li>
-                  <li><a href="http://www.endo-nurses.org/2018-Travel-Grant-Application">Travel Grant Application</a></li>
-                </ul>
-            </li>
-            <li> <SignOutButton /></li>
-          </ul>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav navbar-right">
+              <li><Link to="#myCarousel">HOME</Link></li>
+                <li><Link to="#">SPONSORS</Link></li>
+              <li><Link to="#memberships">MEMBERSHIP</Link></li>
+              <li><Link to="#newEvents">EVENTS</Link></li>
+              <li><Link to="#boardmembers">LEADERSHIP</Link></li>
+              <li><Link to="#contact">CONTACT</Link></li>
+              <li><a href ="https://ens-final.herokuapp.com/">MEMBERS<span className="glyphicon glyphicon-lock"></span></Link></li>
+
+
+              <li className="dropdown">
+                <a className="dropdown-toggle" data-toggle="dropdown" href="">AWARDS
+                <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><a href="http://www.endo-nurses.org/2018-ENS-Grant-Awards">ENS Poster Award</a></li>
+                    <li><a href="http://www.endo-nurses.org/2018-Betsy-Love-McClung-Award">Betsy Love McClung Award</a></li>
+                    <li><a href="http://www.endo-nurses.org/page-1428935">Media</a></li>
+                  </ul>
+              </li>
+              <li className="dropdown">
+                <a className="dropdown-toggle" data-toggle="dropdown" href="#">GRANTS
+                <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><Link to="http://www.endo-nurses.org/ENS-2018">Clinical Project Grant</Link></li>
+                    <li><Link to="http://www.endo-nurses.org/ENS-2018">Clinical Research Grant</Link></li>
+                    <li><Link to="http://www.endo-nurses.org/2018-Travel-Grant-Application">Travel Grant Application</Link></li>
+                  </ul>
+              </li>
+              <li><a href="wwww.google.com"><span className="glyphicon glyphicon-search"></span></a></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-</nav>
+  </nav>
+
+
+  );
 
 
 );
